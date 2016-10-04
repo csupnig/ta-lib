7dc3275efbb1b4b6b9c27dc3741dcb43549580e4
rajkolli03 <rajendraatiitg@gmail.com>
2016-10-05 01:00:43 +0530

    adding percentage price oscillator
    Adding percentage price oscillator indicator to library

--- definitions/index.d.ts
+++ definitions/index.d.ts
@@ -17,6 +17,11 @@ export interface IBBANDSResult {
     lowband: Array<number>;
     middleband: Array<number>;
 }
+export interface IPPOResult {
+    ppo: Array<number>;
+    signal: Array<number>;
+    histogram: Array<number>;
+}
 export declare function arrayMax(array: Array<number>): number;
 export declare function arrayMin(array: Array<number>): number;
 export declare function upscale(number: number, places: number): number;
@@ -31,3 +36,4 @@ export declare function STOCHRSI(instruments: Array<number>, rsiperiod: number):
 export declare function SMA(originalArray: Array<number>, smaLength: number): Array<number>;
 export declare function EMA(originalArray: Array<number>, emaLength: number): Array<number>;
 export declare function MACD(array: Array<number>, i12: number, i26: number, i9: number): IMACDResult;
+export declare function PERCPRICEOSC(array: Array<number>, i12: number, i26: number, i9: number): IPPOResult;
--- test/ta-lib_test.js
+++ test/ta-lib_test.js
@@ -240,4 +240,54 @@ describe('MathUtil', function(){
            assert.equal(Math.round(bbands.highband[0] * 100) / 100,94.15);
        });
     });
+     describe('PERCPRICEOSC', function(){
+        it('should yield expected results', function(){
+           var instruments = [86.16,
+               89.09,
+               88.78,
+               90.32,
+               89.07,
+               91.15,
+               89.44,
+               89.18,
+               86.93,
+               87.68,
+               86.96,
+               89.43,
+               89.32,
+               88.72,
+               87.45,
+               87.26,
+               89.50,
+               87.90,
+               89.13,
+               90.70,
+               92.90,
+               92.98,
+               91.80,
+               92.66,
+               92.68,
+               92.30,
+               92.77,
+               92.54,
+               92.95,
+               93.20,
+               91.07,
+               89.83,
+               89.74,
+               90.40,
+               90.74,
+               88.02,
+               88.09,
+               88.84,
+               90.78,
+               90.54,
+               91.39,
+               90.65].reverse(),
+               ppriceosc = MathUtil.PERCPRICEOSC(instruments,12,26,9);
+            assert.equal(Math.round(ppriceosc.ppo[0] * 100)/100, 0.22);
+            assert.equal(Math.round(ppriceosc.signal[0] * 100)/100, 0.41);
+            assert.equal(Math.round(ppriceosc.histogram[0] * 100)/100 ,-0.19);
+        });
+      });
 });
